75262153a67a2cf089df070573806fe3333fc5f5
Armindo Matias <armindomatiasg@gmail.com>
2023-08-17 18:28:23 +0100

    watch list without review optionals, renders and wo card style

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @movies = @list.movies
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list.id)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :picture_url)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+
+url_serialized = URI.open(url).read
+
+movies = JSON.parse(url_serialized)
+
+movies_results = movies["results"]
+
+movies_results.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+end
